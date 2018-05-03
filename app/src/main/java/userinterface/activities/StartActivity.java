5e90a6f1a38421e48f3f738534fafe12cd399de2
Fryman1991 <fryman1991@gmail.com>
2018-05-02 22:00:06 -0500

    Add launching of Client and Host activities to StartActivity

--- app/src/main/java/userinterface/activities/StartActivity.java
+++ app/src/main/java/userinterface/activities/StartActivity.java
@@ -1,8 +1,10 @@
 package userinterface.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import chrisfry.spotifydj.R;
 
@@ -11,9 +13,27 @@ import chrisfry.spotifydj.R;
  */
 
 public class StartActivity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+
+        findViewById(R.id.btn_host_queue).setOnClickListener(mTypeSelect);
+        findViewById(R.id.btn_join_queue).setOnClickListener(mTypeSelect);
     }
+
+    View.OnClickListener mTypeSelect = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_host_queue:
+                    startActivity(new Intent(StartActivity.this, HostActivity.class));
+                    break;
+                case R.id.btn_join_queue:
+                    startActivity(new Intent(StartActivity.this, QueueConnectActivity.class));
+                    break;
+            }
+        }
+    };
 }
