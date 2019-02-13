7a1b443dc5dfc5df7ebbdd2785e684b5cf540bdc
Fryguy1991 <fryman1991@gmail.com>
2019-02-13 13:13:11 -0600

    Change playback control to use small layout. Create view object PlaybackControlView to manage UI.

---
+++ app/src/main/java/com/chrisfry/socialq/userinterface/views/PlaybackControlView.kt
@@ -0,0 +1,127 @@
+package com.chrisfry.socialq.userinterface.views
+
+import android.content.Context
+import android.os.Build
+import android.transition.TransitionManager
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.chrisfry.socialq.R
+import com.chrisfry.socialq.model.ClientRequestData
+import com.chrisfry.socialq.utils.DisplayUtils
+
+class PlaybackControlView : ConstraintLayout {
+    companion object {
+        val TAG = PlaybackControlView::class.java.name
+    }
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(context, attributeSet, defStyle)
+
+    // UI ELEMENTS
+    // Root constraint layout
+    private val rootLayout: ConstraintLayout
+    // Current track information
+    private val albumArtImage: ImageView
+    private val trackNameText: TextView
+    private val artistNameText: TextView
+    private val userNameText: TextView
+    // Playback control buttons
+    private val playPauseButton: View
+    private val skipButton: View
+
+    // Listener for communicating button presses
+    private lateinit var listener: IPlaybackControlListener
+    // Flag for if the player is currently playing
+    private var isPlaying = false
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.playback_control_layout, this)
+
+        rootLayout = findViewById(R.id.cl_playback_root)
+
+        albumArtImage = findViewById(R.id.cv_playback_control_album_image)
+        trackNameText = findViewById(R.id.tv_playback_control_track_name)
+        artistNameText = findViewById(R.id.tv_playback_control_artist_name)
+        userNameText = findViewById(R.id.tv_playback_control_user_name)
+
+        playPauseButton = findViewById(R.id.btn_playback_control_play_pause)
+        skipButton = findViewById(R.id.btn_playback_control_skip)
+
+        playPauseButton.setOnClickListener {
+            listener.requestPlayPause(isPlaying)
+        }
+
+        skipButton.setOnClickListener {
+            listener.requestSkip()
+        }
+    }
+
+    fun setListener(listener: IPlaybackControlListener) {
+        this.listener = listener
+    }
+
+    fun shrinkLayout() {
+        Log.d(TAG, "Shrinking playback control layout")
+
+        trackNameText.maxLines = 1
+        artistNameText.maxLines = 1
+        userNameText.maxLines = 1
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.beginDelayedTransition(rootLayout)
+        }
+    }
+
+    fun expandLayout() {
+        Log.d(TAG, "Expanding playback control layout")
+
+        trackNameText.maxLines = Integer.MAX_VALUE
+        artistNameText.maxLines = Integer.MAX_VALUE
+        userNameText.maxLines = Integer.MAX_VALUE
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.beginDelayedTransition(rootLayout)
+        }
+    }
+
+    fun setPlaying() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            playPauseButton.background = resources.getDrawable(R.drawable.rectangle_pause_button, context.theme)
+        } else {
+            playPauseButton.background = resources.getDrawable(R.drawable.rectangle_pause_button)
+        }
+        isPlaying = true
+    }
+
+    fun setPaused() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            playPauseButton.background = resources.getDrawable(R.drawable.rectangle_play_button, context.theme)
+        } else {
+            playPauseButton.background = resources.getDrawable(R.drawable.rectangle_play_button)
+        }
+        isPlaying = false
+    }
+
+    fun displayRequest(request: ClientRequestData) {
+        trackNameText.text = request.track.track.name
+        artistNameText.text = DisplayUtils.getTrackArtistString(request.track)
+        userNameText.text = request.user.display_name
+
+        if (request.track.track.album.images.size > 0) {
+            Glide.with(albumArtImage).load(request.track.track.album.images[0].url).into(albumArtImage)
+        }
+    }
+
+    interface IPlaybackControlListener {
+        fun requestPlayPause(isPlaying: Boolean)
+
+        fun requestSkip()
+    }
+}
