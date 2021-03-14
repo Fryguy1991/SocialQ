9101b7bf04ffd8ef2e77778a8c9d00c9fc3f5670
Fryguy1991 <fryman1991@gmail.com>
2021-03-14 15:09:48 -0500

    Update all the things

--- app/src/main/java/com/chrisf/socialq/model/spotify/pager/Pager.kt
+++ app/src/main/java/com/chrisf/socialq/model/spotify/pager/Pager.kt
@@ -1,15 +1,51 @@
 package com.chrisf.socialq.model.spotify.pager
 
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class Pager<Type : Parcelable>(
-        val href: String,
-        val items: List<Type>,
-        val limit: Int,
-        val next: String?,
-        val offset: Int,
-        val previous: String?,
-        val total: Int
-) : Parcelable
+    val href: String,
+    val items: List<Type>,
+    val limit: Int,
+    val next: String?,
+    val offset: Int,
+    val previous: String?,
+    val total: Int
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().orEmpty(),
+        mutableListOf<Type>().also {
+            parcel.readList(it, Pager<Type>::items.javaClass.classLoader)
+        },
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(href)
+        parcel.writeTypedList(items)
+        parcel.writeInt(limit)
+        parcel.writeString(next)
+        parcel.writeInt(offset)
+        parcel.writeString(previous)
+        parcel.writeInt(total)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Pager<Parcelable>> {
+        override fun createFromParcel(parcel: Parcel): Pager<Parcelable> {
+            return Pager(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Pager<Parcelable>?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
